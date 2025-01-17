d81466c25b2ab67fdf2ab51bb09d3145c04323e1
KINGMATP <kingmatp11@gmail.com>
2025-01-17 19:55:52 +0330

    Add files via upload

---
+++ login program/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("enter your username: ");
+var username  = Console.ReadLine();
+Console.Write("enter your password: ");
+var  password =int.Parse(Console.ReadLine());
+if(username == "morteza" && password == 1234)
+{
+    Console.WriteLine("welcome to my application");
+}
+else if(username!="morteza" && password == 1234)
+{
+    Console.Write("username is not valid");
+}
+else if(username == "morteza" && password != 1234)
+{
+    Console.WriteLine("password is not valid");
+}
+else
+{
+    Console.WriteLine("you are not from here arent you?");
+}
