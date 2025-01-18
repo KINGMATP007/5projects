01760354cbccdd598b4a052b8d77cc362a33106f
KINGMATP <kingmatp11@gmail.com>
2025-01-19 00:06:45 +0330

    Add files via upload

---
+++ mohit masahat/Program.cs
@@ -0,0 +1,13 @@
+﻿//ورودی کاربر برای تعیین شعاع
+Console.Write("enter the radius:");
+int r = int.Parse(Console.ReadLine());
+//تعریف عدد پی 
+var pi = 3.14;
+//فرمول محیط 
+var mohit = 2 * pi * r;
+Console.Write($"mohit:{mohit}");
+//فاصله برای زیبایی 
+Console.WriteLine();
+//فرمول مساحت
+var masahat = pi * r * r;
+Console.Write($"masahat:{masahat}");
