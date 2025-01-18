23b40d0effe69bb2cae5178d9cfe949db35e81ec
KINGMATP <kingmatp11@gmail.com>
2025-01-18 23:27:06 +0330

    Add files via upload

---
+++ even numbers from 0 to N/Program.cs
@@ -0,0 +1,13 @@
+﻿//ورودی گرفتن از کاربر برای مشخص کردن طول آرایه
+Console.Write("enter the lentgh:");
+//تبدیل به متغیر عدد صحیح
+int n = int.Parse(Console.ReadLine());
+//حلقه ای برای چاپ اعداد ۰تا n
+for(int i = 0; i <=n; i++)
+{//شرطی برای جداسازی اعداد زوج
+    if(i%2==0)
+    {
+        //چاپ اعداد زوج
+        Console.WriteLine(i);
+    }
+}
